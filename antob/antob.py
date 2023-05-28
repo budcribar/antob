633251c958a396f2384458d48f6389b4a2d04d67
Raymond Cribar <raymond.cribar@hp.com>
2023-05-28 13:18:37 -0600

    WIP

--- antob/antob.py
+++ antob/antob.py
@@ -19,11 +19,12 @@ def get_blazor_extension(extension):
     return extension
 
 
-def copy_and_rename(src, dest):
+def process_files(src, dest):
     # Ensure the destination directory exists
     os.makedirs(dest, exist_ok=True)
 
     skip_files = ['angular.json', 'package.json', 'editorconfig']
+    convert_files = ['cart.component.ts']
     
     for root, dirs, files in os.walk(src):
         # Determine the relative path to the current directory
@@ -42,18 +43,34 @@ def copy_and_rename(src, dest):
             if f in skip_files or fnmatch.fnmatch(f, 'tsconfig*.json'):
                 continue
 
+            if f not in convert_files:
+                continue
+
             # Convert the file name to the Blazor naming convention
             file_name, file_ext = os.path.splitext(f)
             blazor_name = convert_to_blazor_name(file_name) + get_blazor_extension(file_ext)
 
-            # Copy and rename the file in the destination folder
-            shutil.copy2(os.path.join(root, f), os.path.join(dest, relative_path, blazor_name))
+            source = os.path.join(root, f)
+            destination = os.path.join(dest, relative_path, blazor_name)
+
+            with open(source, 'r', encoding='utf-8') as f:
+               file_contents = f.read()
+
+            if destination.endswith('.cs'):
+                message = convert_to_cs(source,file_contents)
+            elif destination.endswith('.razor'):
+                message = convert_to_razor(source,file_contents)           
+            else:
+                message = file_contents
+
+            with open(destination, 'w', encoding='utf-8') as f:
+                f.write(message)
 
 
 
-def convert_to_cs(file_contents):
+def convert_to_cs(path,file_contents):
     model = "text-davinci-002"
-    prompt = "Convert the following angular typescript code into blazor c#:"
+    prompt = "Convert the following angular typescript code into blazor c# using the following hints (1.Add appropriate using statements if needed. 2. Do not include @Component statements):"
 
     completions = openai.Completion.create(
                 engine=model,
@@ -66,7 +83,7 @@ def convert_to_cs(file_contents):
     message = completions.choices[0].text
     return message
 
-def convert_to_razor(file_contents):
+def convert_to_razor(path,file_contents):
     model = "text-davinci-002"
     prompt = "Convert the following angular html code into razor:"
 
@@ -80,36 +97,25 @@ def convert_to_razor(file_contents):
             )
     message = completions.choices[0].text
     return message
+
+def create_project_file(dest):
+    model = "text-davinci-002"
+    prompt = "create a blazor webassembly .csproj file using net7 for <TargetFramework> and use version '7.0.0' for any packages starting with 'Microsoft.AspNetCore.Components'. Make sure the following packages are included 'Microsoft.AspNetCore.Components' and 'Microsoft.AspNetCore.Components.Web' and 'Microsoft.AspNetCore.Components.WebAssembly'. Do not include any xml statements or any project references. also format the result with appropriate line endings"
+    destination = os.path.join(dest, os.path.basename(dest) + ".csproj" )
+    completions = openai.Completion.create(
+                engine=model,
+                prompt=prompt,
+                max_tokens=1024,
+                n=1,
+                stop=None,
+                temperature=0.5,
+            )
+    message = completions.choices[0].text
+    with open(destination, 'w', encoding='utf-8') as f:
+        f.write(message)
     
 
-def process_cs_files(directory):
-    
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith('.cs'):
-                
-                file_path = os.path.join(root, file)
-                
-                with open(file_path, 'r', encoding='utf-8') as f:
-                    file_contents = f.read()
-             
-                message = convert_to_cs(file_contents)
-                            
-                with open(file_path, 'w', encoding='utf-8') as f:
-                    f.write(message)
 
-            if file.endswith('.razor'):
-              
-                file_path = os.path.join(root, file)
-                
-                with open(file_path, 'r', encoding='utf-8') as f:
-                    file_contents = f.read()
-                
-                message = convert_to_razor(file_contents)
-
-                with open(file_path, 'w', encoding='utf-8') as f:
-                    f.write(message)
-               
 
 
 def main():
@@ -117,10 +123,12 @@ def main():
     #destination = input("Enter destination directory: ")
     src_dir = 'C:/Users/Arti_BlizzardPV3/source/repos/Example1'
     dest_dir = 'C:/Users/Arti_BlizzardPV3/source/repos/BlazorExample1'
-    copy_and_rename(src_dir, dest_dir)
+    #copy_and_rename(src_dir, dest_dir)
 
     openai.api_key = os.environ["OPENAI_API_KEY"]
-    process_cs_files(dest_dir)
+
+    #create_project_file(dest_dir)
+    process_files(src_dir,dest_dir)
 
 if __name__ == '__main__':
     main()
